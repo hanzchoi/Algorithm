9e0375327da6a9de0a2a97f86aba8edf8ec7f008
Hanz <personabe1984@gmail.com>
2019-04-23 20:26:27 -0400

    added the fourth solution that uses reduce function and es2015 syntax for the callback

--- extra_practice/reverse_string/practice.js
+++ extra_practice/reverse_string/practice.js
@@ -14,12 +14,31 @@ function reverse(str) {
 }
 
 //Solution 2
-function reverseVersionTwo(){
+function reverseVersionTwo(str){
   let reversedString = "";
   for(let i = str.length-1; i >= 0; i--){
     reversedString += str.charAt(i);
   }
   return reversedString;
+}
+
+// Solution 3
+// very similar to solution two but with for of syntax
+function reverseVersionThree(str){
+  let reversedString = "";
+  for(let character of str){
+    reversedString = character + reversedString;
+  }
+  return reversedString
+}
+
+// Solution 4
+// similar to solution one but uses reduce()
+// first parameter is a callback and second is the init value
+// with in the callback function we have (accumulator, currentValue)
+// also using ES2015 so no return on the callback function
+function reverseVersionFour(str){
+  return str.split('').reduce((rev, char) => char + rev, '')
 }
 
 module.exports = reverse;

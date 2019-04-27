7dc020adc3db699cc33b681e1c7d5166f975d930
Hanz <personabe1984@gmail.com>
2019-04-26 20:44:06 -0400

    added my first solution for the reverse int problem

--- extra_practice/integer_reversal/practice.js
+++ extra_practice/integer_reversal/practice.js
@@ -7,10 +7,24 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+//   console.log(reverseStringInt.charAt(reverseStringInt.length-1))
+
+const intArr = [15, 981, 500, -15, -90];
 
 function reverseInt(n) {
+  let reverseStringInt = n.toString(10).split('').reverse().join('');
+  const lastIndex = reverseStringInt.length - 1;
 
-  
+  if(reverseStringInt.charAt(lastIndex) === "-"){
+    return (
+      parseInt(`${reverseStringInt.charAt(lastIndex)}`
+      +`${parseInt(reverseStringInt.slice(0,lastIndex), 10)}`
+      ,10)
+    )
+  }else{
+    return parseInt(reverseStringInt, 10);
+  }
 }
+
 
 module.exports = reverseInt;

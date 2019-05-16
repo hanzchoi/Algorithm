1da0201e22474f8f143aee004e8a1989747cf220
Hanz <personabe1984@gmail.com>
2019-05-15 22:30:58 -0400

    test

---
+++ extra_practice/reverse_words_but_leave_comma/practice.js
@@ -0,0 +1,11 @@
+const sen = "Hey there, my buddy";
+
+
+function reverseWordsWithComma(sen){
+  let temp = "";
+  let 
+}
+
+console.log(reverseWordsWithComma(sen));
+
+//return sen.split(" ").map(word => word.includes(",") ? word.replace(",","").split("").reverse().join("")+"," : word.split("").reverse().join("")).join(" ");

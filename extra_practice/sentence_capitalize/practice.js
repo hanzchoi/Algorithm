def5689c254006dfff17d6290364eadcb9fdb99e
Hanz <personabe1984@gmail.com>
2019-06-18 14:16:59 -0400

    adding my python practice from udacity

---
+++ extra_practice/sentence_capitalize/practice.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+const test1 = 'a short sentence';
+
+function capitalize(str) {
+  let temp = str.split(' ');
+  for(let word of temp){
+    if(word.length === 1){
+      word = word.toUpperCase();
+    }else{
+      word = word.charAt(0).toUpperCase() + word.slice(1)
+    }
+  }
+  return temp
+
+}
+
+console.log(capitalize(test1))
+
+module.exports = capitalize;
